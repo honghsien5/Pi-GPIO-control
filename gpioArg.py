fc227f0399017c7be86f1bcb69bb82039b3bd2c3
Shien Hong <honghsien5@gmail.com>
2015-04-21 17:23:39 -0700

    add argument version of gpio pin control

---
+++ gpioArg.py
@@ -0,0 +1,35 @@
+#Filename: gpioArg.py
+#Author: Shien Hong 
+#Description: GPIO pin toggle using the arguments
+
+#import libraries
+import RPi.GPIO as GPIO
+import time
+
+#Configure the GPIO pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#Assign LED pin
+
+if len(sys.argv) != 2:
+    raise ValueError("Arm not found")
+
+led = str(sys.argv[1])
+switch = str(sys.argv[2])
+GPIO.setup(led, GPIO.OUT)
+
+#toggle or reset the GPIO pins
+if switch == 'on':
+	GPIO.output(led, 1)
+elif switch == 'off':
+	GPIO.output(led, 0)
+elif switch == 'reset':
+	GPIO.cleanup()
+else:
+	print 'wrong second argument'
+		
+	
+	
+
+
