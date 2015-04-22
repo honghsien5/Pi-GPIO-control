b20bb1d8e10a765aeaaae43007cab44dfba2a5d4
Shien Hong <honghsien5@gmail.com>
2015-04-21 17:27:50 -0700

    add argument version of gpio pin control

--- gpioArg.py
+++ gpioArg.py
@@ -16,7 +16,7 @@ GPIO.setwarnings(False)
 if len(sys.argv) != 3:
     raise ValueError("incorrect argument count")
 
-led = str(sys.argv[1])
+led = sys.argv[1]
 switch = str(sys.argv[2])
 GPIO.setup(led, GPIO.OUT)
 
